110f70ec60a1db4a521877b33d80d21b9a5c4051
Rastislav <rastislav@onion.email>
2025-01-11 16:02:52 +0100

    Html plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';  // Comment out PWA import
+//import { VitePWA } from 'vite-plugin-pwa';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import CONFIG from './gitprofile.config';
 
 // https://vitejs.dev/config/
@@ -8,6 +9,27 @@ export default defineConfig({
   base: CONFIG.base || '/',
   plugins: [
     react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          metaTitle: CONFIG.seo.title,
+          metaDescription: CONFIG.social.coreid ? CONFIG.seo.description + ' / Core ID: ' + CONFIG.social.coreid : CONFIG.seo.description,
+          metaImageURL: CONFIG.seo.githubId ? 'https://avatars.githubusercontent.com/u/' + CONFIG.seo.githubId : CONFIG.seo.imageURL,
+          metaPaytoProperty: CONFIG.seo.payto?.property || '',
+          metaPaytoContent: CONFIG.seo.payto?.content || '',
+          googleAnalytics: CONFIG.googleAnalytics.id ? `
+            <script async src="https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.id}"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', '${CONFIG.googleAnalytics.id}');
+            </script>
+          ` : '',
+        },
+      },
+    }),
+
     // Temporarily remove PWA plugin
   ],
   define: {
